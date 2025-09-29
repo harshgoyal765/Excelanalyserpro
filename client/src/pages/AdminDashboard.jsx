f0afc18e5c8ddb98cfa587a2cc1b2025eeb6637f
harsh goyal <18harshgoyal@gmail.com>
2025-09-30 00:32:05 +0530

    update

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
-import TopBar from '../components/Topbar';
+
+import TopBar from '../components/TopBar';
 import StatsCards from '../components/StatsCards';
 import ChartsSection from '../components/ChartsSection';
 import UploadStatistics from '../components/UploadStatistics';
@@ -10,7 +10,7 @@ export default function AdminDashboard() {
     <div className="flex h-screen bg-gray-100">
       
       <main className="flex-1 p-6 overflow-auto">
-        <TopBar />
+        <TopBar/>
         <StatsCards />
         <ChartsSection />
         <UploadStatistics />
